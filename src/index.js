6c1159defea29eb94f5a6e0a919c2617a1a80424
Kajals22 <kajal@codenomad.net>
2022-09-14 17:34:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ var scene = new THREE.Scene();
 
 // Camera Object
 
+// adding first cube...
 var camera = new THREE.PerspectiveCamera(2, window.innerWidth / window.innerHeight, 3, 1000);
 camera.position.set(-30,40,40)
 camera.lookAt(scene.position);
@@ -42,7 +43,16 @@ cube.rotation.set(0, 0, 0);
 scene.add(cube);
 
 
-// Making StandoffTexture
+// adding second frame...
+
+var geome = new THREE.BoxGeometry(0.1,2.5,2.5);
+var mater = new THREE.MeshBasicMaterial({transparent:false});
+var mesh = new THREE.Mesh(geome , mater);
+cube.add(mesh);
+mesh.position.set(0, 0, 0);
+
+
+// Making StandoffTexturee
 
 
 
@@ -61,7 +71,7 @@ cube.add(standoff2);
 geom2.rotateZ(-Math.PI * 0.5);
 standoff2.position.set(0,1.2,-1.3);
 
-var geom3 = new THREE.CylinderGeometry( 0.1,0.1,0.3,64 );
+var geom3 = new THREE.CylinderGeometry( 0.1,0.1,0.3 ,64 );
 var mate3 = new THREE.MeshBasicMaterial({color: 0x000});
 var standoff3 = new THREE.Mesh(geom3 , mate3);
 cube.add(standoff3);
@@ -130,9 +140,12 @@ var options = {
     cube.scale.x = 1;
     cube.scale.y = 1;
     cube.scale.z = 1;
+    mesh.scale.x = 1;
+    mesh.scale.y = 1;
+    mesh.scale.z = 1;
   },
   reset: function() {
-    this.velx = 0;
+    this.velx = 0;   
     this.vely = 0;
     camera.position.z = 200;
     camera.position.x = 400;
@@ -141,16 +154,26 @@ var options = {
     cube.scale.y = 1;
     cube.scale.z = 1;
     cube.material.color.set(0xffffff);
+    mesh.scale.x = 1;
+    mesh.scale.y = 1;
+    mesh.scale.z = 1;
+    material = new THREE.MeshBasicMaterial({map:0xffffff})
+    mesh.material =material;
+    wireframe.material.color.set(0x000000)
+    standoff.material.color.set(0x000000)
+    standoff2.material.color.set(0x000000)
+    standoff3.material.color.set(0x000000)
+    standoff4.material.color.set(0x000000)
+  
   },
-};
+};  
 
-// DAT.GUI Related Stuff
 
-var gui = new GUI();
+var gui = new GUI();   
 
 var cam = gui.addFolder('Camera');
-cam.add(options.camera, 'speed', 0, 0.0010).listen();
-cam.add(camera.position, 'y', 0, 1000).listen();
+// cam.add(options.camera, 'speed', 0, 0.0010).listen();
+cam.add(camera.position,'y', 0, 1000).listen();
 cam.open();
 
 
@@ -159,7 +182,7 @@ cam.open();
 
 var box = gui.addFolder('Cube' );
 box.add(cube.scale, 'x', 0, 0.1).name('Width');//.listen();
-box.add(cube.scale, 'y', 0, 3).name('Height');//.listen();
+box.add(cube.scale, 'y', 0, 3 ).name('Height');//.listen();
 box.add(cube.scale, 'z', 0, 6).name('Length');//.listen();
 
 box.open();
@@ -180,11 +203,15 @@ abc.open();
 var image = gui.addFolder('Add-Image')
 image.open();
 var GuiConfig = function  () {
-    this['Image Path'] = 'img-path';  // default image path
+    this['Image Path'] = 'Image-Path';  // default image path
+    image.src = 'webgl/mdm_webgl_20.png';
 
     this['Upload Image'] = function(upload) {
         // you need to create an input element in HTML
-        var input = document.getElementById('img-path'); 
+        var input = document.getElementById('img-path');
+        // input.appendChild(image);
+        // console.log(input.appendChild(image));
+        // console.log(image.src)
         input.addEventListener('change', function(e) {
              var file = e.target.files[0];
           
@@ -194,6 +221,7 @@ var GuiConfig = function  () {
                new THREE.TextureLoader().load(reader.result ,function onLoad(texture){
                 // const line = new THREE.LineSegments( wireframe );
               // console.log('LINE', texture)
+              // this['Image Path'] = 'reader.result';  // default image path
 
               
               var material = [
@@ -213,7 +241,7 @@ var GuiConfig = function  () {
               // scene.add(cube1)
               console.log(cube)
               material.depthtset = false;
-              cube.material = material;
+              mesh.material = material;
               // wireframe.push(cube);
               material.map= texture
               //material.map.needsupdate = true;
@@ -236,30 +264,40 @@ var GuiConfig = function  () {
 
           
 var config = new GuiConfig();
-gui.add(config, 'Image Path', config['Image Path']);
+gui.add(config, 'Image Path', config['reader.result']);
 gui.add(config, 'Upload Image');
 // gui.add(config, 'Save Png')
 
 
 
 gui.add(options, 'stop');
-gui.add(options, 'reset');
-
-
+gui.add(options, 'reset');// onClick="refreshPage()");
+// var guiReset = function(){
+//   this['Reset'] = function(res){
+// var button = document.getElementsByClassName(".reset");
+//   console.log(button);
+//   for (var i = 0; i < button.length; i++) {
+//   button.addEventListener("onClick" ,function(e) {
+// window.location.reload();
+// });
+//   }
+// }}
+// var cot = new guiReset();
+// gui.add(cot, 'Reset')
 //WIreFrame Color Settings
 
 var geo = new THREE.EdgesGeometry( cube.geometry )
 var mat = new THREE.LineBasicMaterial({ color :0x000000 ,	linewidth: 2,})
 
 var wireframe = new THREE.LineSegments( geo, mat );
-cube.add( wireframe)
+cube.add(wireframe)
 
 //Wireframe color Setting GUI
 
   var cde = gui.addFolder('Frame Color' );
-  var confi = { color : '#ffae23' };    
-  gui.addColor(confi, 'color').onChange( function(colorValue2) {
-    wireframe.material.color.set(colorValue2, 'color')
+  var confi = { colour : '#ffae23' };    
+  gui.addColor(confi, 'colour').onChange( function(colorValue2) {
+    wireframe.material.color.set(colorValue2, 'colour')
 
 
   cde.open();
@@ -269,51 +307,51 @@ cube.add( wireframe)
 
 // dropdown
 
-const guiOptions = {
-  SelectTextures: "Textures",
-};
+// const guiOptions = {
+//   SelectTextures: "Textures",
+// };
 
-const textureloader = new THREE.TextureLoader();
-const texture1 = textureloader.load('../textures/proxy.jpeg');
-const texture2 = textureloader.load('../textures/proxy2.jpeg');
-const texture3 = textureloader.load('../textures/proxy3.jpeg');
-const texture4 = textureloader.load('../textures/proxy4.jpeg');
-const texture5 = textureloader.load('../textures/proxy5.jpeg');
+// const textureloader = new THREE.TextureLoader();
+// const texture1 = textureloader.load('../textures/proxy.jpeg');
+// const texture2 = textureloader.load('../textures/proxy2.jpeg');
+// const texture3 = textureloader.load('../textures/proxy3.jpeg');
+// const texture4 = textureloader.load('../textures/proxy4.jpeg');
+// const texture5 = textureloader.load('../textures/proxy5.jpeg');
 
-// console.log('texture2', texture2);  
-const guiTextureHash = {
-    Texture1:texture1,
-    Texture2:texture2,
-    Texture3:texture3,
-    Texture4:texture4,
-    Texture5:texture5,
-};
-/* Add to gui */
-gui.add(guiOptions, "SelectTextures", Object.keys(guiTextureHash)).onChange((value) => {
-// new THREE.TextureLoader().load(value, function (texture) {
-  var material = [
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshBasicMaterial({map:value, transparent: false}),
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshBasicMaterial(),
-   ]
-  //  var mat = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 4 } );
-  //  var wireframe = new THREE.LineSegments( geometry, material );
-  //  wireframe.renderOrder = 1; // make sure wireframes are rendered 2nd
-  //  cube.add( wireframe )
-  console.log(material)
-  // const cube = new THREE.Mesh(geometry , mat)
-  // scene.add(cube1)
-  console.log(cube)
-  material.depthtset = false;
-  cube.material = material;
-  // wireframe.push(cube);
-  material.map= value
-  //material.map.needsupdate = true;
-})
+// // console.log('texture2', texture2);  
+// const guiTextureHash = {
+//     Texture1:texture1,
+//     Texture2:texture2,
+//     Texture3:texture3,
+//     Texture4:texture4,
+//     Texture5:texture5,
+// };
+// /* Add to gui */
+// gui.add(guiOptions, "SelectTextures", Object.keys(guiTextureHash)).onChange((value) => {
+// // new THREE.TextureLoader().load(value, function (texture) {
+//   var material = [
+//     new THREE.MeshBasicMaterial(),
+//     new THREE.MeshBasicMaterial({map:value, transparent: false}),
+//     new THREE.MeshBasicMaterial(),
+//     new THREE.MeshBasicMaterial(),
+//     new THREE.MeshBasicMaterial(),
+//     new THREE.MeshBasicMaterial(),
+//    ]
+//   //  var mat = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 4 } );
+//   //  var wireframe = new THREE.LineSegments( geometry, material );
+//   //  wireframe.renderOrder = 1; // make sure wireframes are rendered 2nd
+//   //  cube.add( wireframe )
+//   console.log(material)
+//   // const cube = new THREE.Mesh(geometry , mat)
+//   // scene.add(cube1)
+//   console.log(cube)
+//   material.depthtset = false;
+//   cube.material = material;
+//   // wireframe.push(cube);
+//   material.map= value
+//   //material.map.needsupdate = true;
 // })
+// // })
 
 
 var saveLink = document.createElement('div');
